144c6d1634df3fc2e3cd692dcf79ea8b856aae0c
Caio dos Anjos <coa.anjos@gmail.com>
2021-06-02 23:30:20 -0300

    Add routes to another palettes

--- src/Palette.js
+++ src/Palette.js
@@ -7,26 +7,36 @@ import './Palette.css';
 export default class Palette extends Component {
     constructor(props) {
         super(props);
-        this.state = { level: 500 }
+        this.state = { level: 500, format: 'hex' }
         this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
     }
+
     changeLevel(level) {
-        this.setState({ level })
+        this.setState({ level });
     }
+
+    changeFormat(value) {
+        this.setState({ format: value })
+    }
+
     render() {
-        const { colors } = this.props.palette;
-        const { level } = this.state;
+        const { colors, paletteName, emoji } = this.props.palette;
+        const { level, format } = this.state;
         const colorBoxes = colors[level].map(color => {
-            return <ColorBox background={color.hex} name={color.name} />
+            return <ColorBox background={color[format]} name={color.name} key={color.id} />
         })
 
         return (
             <div className="Palette">
-                <Navbar level={level} changeLevel={this.changeLevel}/>
+                <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
-
+                <footer className='Palette-footer'>
+                    {paletteName}
+                    <span className='emoji'>{emoji}</span>
+                </footer>
             </div>
         )
     }
